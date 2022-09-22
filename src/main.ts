5dbe20135daf49064ef0a7c9c7466e1fbc8cec21
@YaroslavBerkuta <yaroslavberkuta@gmail.com>
2022-09-22 14:40:23 +0300

    port update

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import {AppModule} from "./app.module";
 
 const start = async () => {
     try {
-        const PORT = process.env.PORT || 5000;
+        const PORT = process.env.PORT || 80;
         const app = await NestFactory.create(AppModule);
         app.enableCors()
         await app.listen(PORT, () => console.log(`server started on PORT ${PORT}`))
